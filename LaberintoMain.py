26a737872241c7c90ca0aa8e0792da9ceb50c823
Diegowich <deagle.sanctum@gmail.com>
2025-03-22 11:21:21 -0500

    Añadida documentacion

--- LaberintoMain.py
+++ LaberintoMain.py
@@ -1,60 +1,60 @@
-from funciones import *
-from os import system
+from funciones import * 
+from os import system  # Importamos la funcion system del modulo os para limpiar la pantalla
 
-volver_jugar = ""
-while volver_jugar != "n":
-    print("Bienvenido al laberinto")
+volver_jugar = ""       # Volver a jugar inicia vacio
+while volver_jugar != "n":      # Mientras volver a jugar no sea n, se ejecutara el juego
+    print("Bienvenido al laberinto")        
     print("Tu objetivo es llegar a la salida 'E'")
 
 
-    tiene_partida_guardada = input("Tienes una partida guardada? (s/n): ")
-    if tiene_partida_guardada == "s":
+    tiene_partida_guardada = input("Tienes una partida guardada? (s/n): ")  # Preguntamos si tiene una partida guardada
+    if tiene_partida_guardada == "s":     # Si tiene una partida guardada, cargamos la partida
         laberinto = cargar_partida()
-    else:
+    else:                                   # Si no tiene una partida guardada, creamos un laberinto nuevo
         dificultad = input("Selecciona la dificultad (facil, medio, dificil): ")
-        laberinto = crear_laberinto(dificultad)
+        laberinto = crear_laberinto(dificultad)         # Creamos el laberinto segun la dificultad seleccionada
         
 
-    eleccion_usuario = ""
+    eleccion_usuario = ""     # La eleccion del usuario inicia vacia
 
-    while eleccion_usuario != "end":
+    while eleccion_usuario != "end":  # Mientras el usuario no escriba end en la eleccion, se ejecutara el juego
         for fila in laberinto:
-            print(" ".join(fila))
+            print(" ".join(fila))    # Se imprime el laberinto seleccionado
         
         eleccion_usuario = input(""" 'W' (arriba) 'A'(izquierda) 'S'(abajo) 'D'(derecha) 'G' (Guardar progreso), 'end' (Salir)"
-    Selecciona una opcion: """).lower().strip()
+    Selecciona una opcion: """).lower().strip()    # Se pide la eleccion del usuario
         
         match eleccion_usuario:
-            case "w":
+            case "w":               # Si la eleccion del usuario es w, se mueve hacia arriba
                 mover_W(laberinto)
-                system("cls")
+                system("cls")           # system cls limpia la pantalla para mostrar el laberinto tras el movimiento hecho
                 
-            case "a":
+            case "a":               # si la eleccion del usuario es a, se mueve hacia la izquierda
                 mover_A(laberinto)
-                system("cls")
-            case "s":
+                system("cls")       # system cls limpia la pantalla para mostrar el laberinto tras el movimiento hecho
+            case "s":               # si la eleccion del usuario es s, se mueve hacia abajo
                 mover_S(laberinto)
-                system("cls")
-            case "d":
+                system("cls")       # system cls limpia la pantalla para mostrar el laberinto tras el movimiento hecho
+            case "d":               # si la eleccion del usuario es d, se mueve hacia la derecha
                 mover_D(laberinto)
-                system("cls")
-            case "g":
+                system("cls")       # system cls limpia la pantalla para mostrar el laberinto tras el movimiento hecho
+            case "g":               # si la eleccion del usuario es g, se guarda la partida
                 mensaje = guardar_partida(laberinto)
-                system("cls")
-                print(mensaje)
+                system("cls")       # system cls limpia la pantalla para mostrar el mensaje de guardado a continuacion
+                print(mensaje)      # Se imprime el mensaje de guardado
                 
-            case "end":
-                system("cls")
-                print("Gracias por jugar")
+            case "end":             # si la eleccion del usuario es end, termina el juego antes de llegar a la salida
+                system("cls")               # Se limpia la pantalla
+                print("Gracias por jugar")  # Se imprime un mensaje de despedida
             
-        if laberinto[-2][-2] == "P":
+        if laberinto[-2][-2] == "P":        # Si la posicion del jugador es igual a la salida, se imprime un mensaje de felicitacion
             print("Felicidades, has llegado a la salida")
-            borrar_guardado()
-            break
+            borrar_guardado()                 # Se borra la partida guardada
+            break   # Se rompe el ciclo while
                 
-    volver_jugar = input("Quieres volver a jugar? (s/n): ")
-    system("cls")
-    print("Hasta luego")
+    volver_jugar = input("Quieres volver a jugar? (s/n): ")   # Se pregunta si se quiere volver a jugar
+    system("cls")       # Se limpia la pantalla
+    print("Hasta luego")        # Si no quiere volver a jugar, se imprime un mensaje de despedida
 
         
         
--- funciones.py
+++ funciones.py
@@ -1,49 +1,49 @@
-from pathlib import Path
-from random import *
+from pathlib import Path            # Importamos la librería Path para crear el directorio y el archivo de guardado
+from random import *                # Importamos la funcion random para generar el mapa aleatorio
 
-def guardar_partida(laberinto):
-    directorio_saves = Path(__file__).parent / "saves"
-    directorio_saves.mkdir(exist_ok= True)
+def guardar_partida(laberinto):                         # Funcion para guardar la partida, que recibe el tipo de laberinto
+    directorio_saves = Path(__file__).parent / "saves"  # Se establece el directorio en donde se guardara la partida, se llamara saves
+    directorio_saves.mkdir(exist_ok= True)              # Se crea el directorio si no existe
     
-    archivo_guardado = directorio_saves / "save.txt"
-    archivo_guardado.write_text(str(laberinto))
-    return(f"Partida guardada en {archivo_guardado}")
+    archivo_guardado = directorio_saves / "save.txt"    # Se establece el archivo de guardado, se llamara save.txt
+    archivo_guardado.write_text(str(laberinto))         # Se escribe el laberinto en el archivo de guardado como un string
+    return(f"Partida guardada en {archivo_guardado}")   # Se devuelve un mensaje de guardado con la ruta del archivo, para notificar al jugador donde se guardo su partida
 
-def cargar_partida():
-    directorio_saves = Path(__file__).parent / "saves"
-    archivo_guardado = directorio_saves / "save.txt"
+def cargar_partida():   # Funcion para cargar la partida
+    directorio_saves = Path(__file__).parent / "saves"      # Se establece la ruta donde debera encontrar el archivo de guardado
+    archivo_guardado = directorio_saves / "save.txt"        # El archivo de guardado que debe buscar es save.txt
     
-    if archivo_guardado.exists():
-        return eval(archivo_guardado.read_text())
+    if archivo_guardado.exists():                   # Si el archivo de guardado existe 
+        return eval(archivo_guardado.read_text())       # Se devuelve el contenido del archivo de guardado como una lista
     else:
-        print("No hay una partida guardada")
-        return None
+        print("No hay una partida guardada")            # Si no hay una partida guardada, se notifica que no hay una partida guardada
+        return None                                     # retorna None para que el programa principal sepa que no hay una partida guardada
     
-def borrar_guardado():
-    directorio_saves = Path(__file__).parent / "saves"
-    archivo_guardado = directorio_saves / "save.txt"
+def borrar_guardado():                      # Funcion para borrar la partida guardada
+    directorio_saves = Path(__file__).parent / "saves"  # Se indica el directorio donde se guardo la partida
+    archivo_guardado = directorio_saves / "save.txt"    # se indica cual es el archivo de guardado que debe buscar
     
-    if archivo_guardado.exists():
-        archivo_guardado.unlink()
-        return "Partida borrada"
+    if archivo_guardado.exists():               # Si el archivo de guardado existe
+        archivo_guardado.unlink()               # Se borra el archivo de guardado
+        return "Partida borrada"                # Se devuelve un mensaje de que la partida fue borrada
     else:
-        return "No hay una partida guardada"
+        return "No hay una partida guardada"    # Si no hay una partida guardada, se devuelve un mensaje de que no hay una partida guardada
     
  
-def dificultad_dificil():
+def dificultad_dificil():           # Funcion para crear laberinto dificultad dificil
     return ""
  
     
-def crear_laberinto(dificultad):
-    if dificultad == "facil":
-        return [
-    ['#', '#', '#', '#', '#'],
+def crear_laberinto(dificultad):        # Funcion para crear el laberinto segun la dificultad seleccionada
+    if dificultad == "facil":           # El laberinto recibe como parametro la dificultad seleccionada
+        return [                    
+    ['#', '#', '#', '#', '#'],          # Si el usuario escribe facil, se retorna un laberinto facil
     ['#', 'P', '.', '.', '#'],
     ['#', '.', '#', '.', '#'],
     ['#', '.', '.', 'E', '#'],
     ['#', '#', '#', '#', '#']
 ]
-    elif dificultad == "medio":
+    elif dificultad == "medio":         # Si el usuario escribe medio, se retorna un laberinto medio
         return [
     ['#', '#', '#', '#', '#', '#', '#'],
     ['#', 'P', '.', '.', '.', '.', '#'],
@@ -52,44 +52,44 @@ def crear_laberinto(dificultad):
     ['#', '#', '#', '#', '#', 'E', '#'],
     ['#', '#', '#', '#', '#', '#', '#']
 ]
-    elif dificultad == "dificil":
+    elif dificultad == "dificil":           # Si el usuario escribe dificil, se retorna un laberinto dificil
         return dificultad_dificil()
       
     
     
-def mover_W(laberinto):
+def mover_W(laberinto):                     # Funcion para mover al jugador hacia arriba
     for i, fila in enumerate(laberinto):
-        if "P" in fila:
-            indice_P = fila.index("P")
-            if laberinto[i-1][indice_P] != "#":
-                laberinto[i][indice_P] = "."
-                laberinto[i-1][indice_P] = "P"
+        if "P" in fila:                     # Se busca la posicion del jugador en el laberinto
+            indice_P = fila.index("P")      # Se obtiene el indice del jugador en la fila
+            if laberinto[i-1][indice_P] != "#": # Si la posicion de arriba del jugador no es una pared
+                laberinto[i][indice_P] = "."    # Se cambia la posicion del jugador a un espacio vacio
+                laberinto[i-1][indice_P] = "P"  # Se cambia la posicion de arriba del jugador a la posicion del jugador
                 break
     
-def mover_A(laberinto):
-    for i, fila in enumerate(laberinto):
-        if "P" in fila:
-            indice_P = fila.index("P")
-            if laberinto[i][indice_P-1] != "#":
-                laberinto[i][indice_P] = "."
-                laberinto[i][indice_P-1] = "P"
+def mover_A(laberinto):                     # Funcion para mover al jugador hacia la izquierda
+    for i, fila in enumerate(laberinto):    
+        if "P" in fila:                     # Se busca la posicion del jugador en el laberinto
+            indice_P = fila.index("P")      # Se obtiene el indice del jugador en la fila
+            if laberinto[i][indice_P-1] != "#": # Si la posicion de la izquierda del jugador no es una pared
+                laberinto[i][indice_P] = "."    # Se cambia la posicion del jugador a un espacio vacio
+                laberinto[i][indice_P-1] = "P"  # Se cambia la posicion de la izquierda del jugador a la posicion del jugador
                 break
 
-def mover_S(laberinto):
-    for i, fila in enumerate(laberinto):
-        if "P" in fila:
-            indice_P = fila.index("P")
-            if laberinto[i+1][indice_P] != "#":
-                laberinto[i][indice_P] = "."
-                laberinto[i+1][indice_P] = "P"
+def mover_S(laberinto):                     # Funcion para mover al jugador hacia abajo
+    for i, fila in enumerate(laberinto):   
+        if "P" in fila:                     # Se busca la posicion del jugador en el laberinto
+            indice_P = fila.index("P")      # Se obtiene el indice del jugador en la fila
+            if laberinto[i+1][indice_P] != "#": # Si la posicion de abajo del jugador no es una pared
+                laberinto[i][indice_P] = "."    # Se cambia la posicion del jugador a un espacio vacio
+                laberinto[i+1][indice_P] = "P"  # Se cambia la posicion de abajo del jugador a la posicion del jugador
                 break
     
     
-def mover_D(laberinto):
+def mover_D(laberinto):                 # Funcion para mover al jugador hacia la derecha
     for i, fila in enumerate(laberinto):
-        if "P" in fila:
-            indice_P = fila.index("P")
-            if laberinto[i][indice_P+1] != "#":
-                laberinto[i][indice_P] = "."
-                laberinto[i][indice_P+1] = "P"
+        if "P" in fila:                 # Se busca la posicion del jugador en el laberinto
+            indice_P = fila.index("P")  # Se obtiene el indice del jugador en la fila
+            if laberinto[i][indice_P+1] != "#": # Si la posicion de la derecha del jugador no es una pared
+                laberinto[i][indice_P] = "."    # Se cambia la posicion del jugador a un espacio vacio
+                laberinto[i][indice_P+1] = "P"  # Se cambia la posicion de la derecha del jugador a la posicion del jugador
                 break
